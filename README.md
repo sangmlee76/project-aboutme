9de11df14fc4dee1d7b31ac158ec5adcab2682d1
Sang Lee <sanglee76@yahoo.com>
2020-10-28 23:51:12 -0700

    updated with Lab 03

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # LAB - 02
 
 ## Project "About Me"
-A web page that provides information about its author using a yes/no Q&A. The webpage asks for the user's name which is used to customize the responses back to the user using an *alert* dialog box. It provides 5 yes/no question prompts to the user, and then provides the user with feedback on their answer.
+A web page that provides information about its author using an interactive Q&A game. 
+
+### Features (by version and release date)
+**version 1.1, released on 28 Oct 2020:** The questionnaire was updated with two new Q&A games. First is a number guessing game; second is a guess my favorite Halloween treat game. It also added a score tracker to provide to user at the end of the game.
+
+**version 1.0, released on 27 Oct 2020:** The webpage asks for the user's name which is used to customize the responses back to the user using an *alert* dialog box. It provides 5 yes/no question prompts to the user, and then provides the user with feedback on their answer.
 
 ## Author: Sang Lee | sanglee76@yahoo.com
 
@@ -9,7 +14,15 @@ A web page that provides information about its author using a yes/no Q&A. The we
 + [submission PR](https://gen.xyz/) **(I'm not quite sure what this is - will inquire with instructor for Lab03)**
 + https://overapi.com/css
 
-## Reflections and Comments
+## Reflections and Comments (with Release label)
+28 Oct 2020 Release (Lab 3):
++ Was able to get most HTML and JS update within couple of hours. Then spent couple of hours debugging my JS to finally get it to work. What got me were:
+  - The task on providing the final message if the user exhausted their attempts. Not intuitive, for me, but once I figured it out, it totally made sense -- though I am unsure if this is the best/right approach.
+  - The proper iteration set-up for my FOR-loop
+  - Figuring out missing parenthesis; this alone took about an hour (no TA was available to help while I was working on it). At one point, I started to mess with my loop logic, but then stopped myself and then drew a flow chart diagram to make sure my logic was sound. And because my flow was logical, I suspected syntax error -- luckily, I was right and was able to figure it out going line by line -- will need to find out if there is a more efficient way to identify these types of syntax error rather than going line by line.
++ Overall, very satisfying lab and great learnings. Unfortunately, due to time restrictions, wasn't able to do much was CSS style improvements or attempt the stretch goals.
+
+27 Oct 2020 Release (Lab 2):
 + Building on 102; I found the exercise of "by the numbers" (military slang) approach to HTML/CSS/JS. As an example, just the introducion of reset.css to forego the basic formatting offered by browser to make sure I, as a developer, have 'full control' of the style and use CSS to execute that style was very instructive and insightful. 
 + One thing that I have **not** been doing is reviewing the end of chapter code examples in detail -- I go back to them, if time permits, but in trying to get the core reading in, I have been using it as a stretch goal. Based on my most recent reading, however, I need to find a way to incorporate this into my core reading.
 + I misunderstood part of the instruction about the customized greeting and farewell message. It was good to explore trying to incorporate that onto the page (e.g. I tried with using `document.write()` but was not able to do it.  I was thankful for the 15 minute rule, so I didn't spend too much time chasing that rabbit hole after confirming with the TA that this was not part of the assignment.
--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
-//Story 2, Task 1 - ask user name through a prompt
+//[Story 2, Task 1] ask user name through a prompt
 var userName = prompt('Hi, what is your name?');
+var correctAnswerCounter = 0;
 
-//Story 2, Task 2 - display the user name through a custom greeting on the webpage
-alert('Welcome ' + userName + '. Thanks for taking the time to play a quick game to learn a little about me. I\'m going to ask you 5 "yes/no" questions. When you are ready to play, click "OK"')
+//[Story 2, Task 2] display the user name through a custom greeting on the webpage
+alert('Welcome ' + userName + '. Thanks for taking the time to play a quick game to learn a little about me. I\'m going to ask you 5 "yes/no" questions. When you are ready to play, click "OK"');
 
-//Story 1, Task 2 - user prompt for 5 yes/no questions and provide alert with feedback about the answer
+//[Story 1, Tasks: 2, 3, 4] user prompt for 5 yes/no questions and provide alert with feedback about the answer
 var firstQuestion = prompt('Was Sang born and raised in Seattle?');
 if(firstQuestion.toLowerCase() === 'yes' || firstQuestion.toLowerCase() === 'y'){
   //console.log('Sorry, your answer is incorrect.');
@@ -14,6 +15,7 @@ if(firstQuestion.toLowerCase() === 'yes' || firstQuestion.toLowerCase() === 'y')
 } else if(firstQuestion.toLowerCase() === 'no' || firstQuestion.toLowerCase() === 'n'){
   //console.log('Your answer is correct - well done!');
   alert('Your answer is correct - well done!');
+  correctAnswerCounter++;
 } else{
   //console.log('Please answer "yes / no" or "y / n"; moving on to the next question');
   alert('Please answer "yes / no" or "y / n"; moving on to the next question');
@@ -26,6 +28,7 @@ if(secondQuestion.toLowerCase() === 'yes' || secondQuestion.toLowerCase() === 'y
 } else if(secondQuestion.toLowerCase() === 'no' || secondQuestion.toLowerCase() === 'n'){
   //console.log('Your answer is correct - good job!');
   alert('Your answer is correct - good job!');
+  correctAnswerCounter++;
 } else{
   //console.log('Please answer "yes / no" or "y / n"; moving on to the next question');
   alert('Please answer "yes / no" or "y / n"; moving on to the next question');
@@ -38,6 +41,7 @@ if(thirdQuestion.toLowerCase() === 'yes' || thirdQuestion.toLowerCase() === 'y')
 } else if(thirdQuestion.toLowerCase() === 'no' || thirdQuestion.toLowerCase() === 'n'){
   //console.log('Your answer is correct - impressive!');
   alert('Your answer is correct - impressive!');
+  correctAnswerCounter++;
 } else{
   //console.log('Please answer "yes / no" or "y / n"; moving on to the next question');
   alert('Please answer "yes / no" or "y / n"; moving on to the next question');
@@ -50,6 +54,7 @@ if(fourthQuestion.toLowerCase() === 'yes' || fourthQuestion.toLowerCase() === 'y
 } else if(fourthQuestion.toLowerCase() === 'no' || fourthQuestion.toLowerCase() === 'n'){
   //console.log('Your answer is correct - you nailed it!');
   alert('Your answer is correct - you nailed it!');
+  correctAnswerCounter++;
 } else{
   //console.log('Please answer "yes / no" or "y / n"; moving on to the next question');
   alert('Please answer "yes / no" or "y / n"; moving on to the next question');
@@ -59,6 +64,7 @@ var fifthQuestion = prompt('Was Sang living in Atlanta during the 1996 Olympic G
 if(fifthQuestion.toLowerCase() === 'yes' || fifthQuestion.toLowerCase() === 'y'){
   //console.log('Your answer is correct - awesome work!');
   alert('Your answer is correct - awesome work!');
+  correctAnswerCounter++;
 } else if(fifthQuestion.toLowerCase() === 'no' || fifthQuestion.toLowerCase() === 'n'){
   //console.log('Sorry, your answer is incorrect.');
   alert('The answer is incorrect.');
@@ -67,6 +73,68 @@ if(fifthQuestion.toLowerCase() === 'yes' || fifthQuestion.toLowerCase() === 'y')
   alert('This was a "yes / no" or "y / n" question; this was the last question');
 }
 
+//[Story 4, Tasks: 1, 2, 3, 4] adds question 6 that uses a number guessing game with a series of user inputs and feedbacks to the user - user gets 4 attempts 
+alert('Shall-we-play-a-game? Let\'s see if you can guess my favorite number; it\'s between 1 and 100. You\'ll have exactly 4 attempts to guess it correctly, but I will give you a hint after each incorrect guess. Click "OK" when you\'re ready to begin.');
 
-//Story 2, Task 3 - display user's name in the final message to the user
-alert('Thank you for stopping by ' + userName + '! I hope you enjoyed learning a little about me. Have a great day!')
+var correctNumber = 55;  // the correct answer to the question
+var allowedAttemptsForQuestion6 = 4; // permitted attempts for the user guesses
+
+// TO-DO: currently the loop is not working at 100%; it goes through the loop 4 times but it will not check the final guess to see if it is correct -- seems like it doesn't enter the loop. I haven't been able to figure out how to work around this after several attempts.
+
+for(var i=0; i <= allowedAttemptsForQuestion6; i++){
+  if(i === allowedAttemptsForQuestion6){
+    alert('Those were great guesses but you have exhausted your guesses. The correct answer is ' + correctNumber);
+    break;
+  }
+  
+  var sixthQuestion = parseInt(prompt('What is my favorite number? (choose between 1 and 100):'));
+  console.log('What is my favorite number? (choose between 1 and 100):');
+  if(sixthQuestion === correctNumber){  // conditional check between user input and the correct answer
+    alert('Amazing! You must be a Jedi -- that\'s the correct answer!');
+    correctAnswerCounter++;
+    break;
+  } else if(sixthQuestion < correctNumber){
+    alert('Sorry, that\'s too low. Go higher! (choose between 1 and 100):');
+  } else if(sixthQuestion > correctNumber){
+    alert('Sorry, that\'s too high. Go lower! (choose between 1 and 100):');
+  } // TO-DO: need ot add an alert to display the correct answer if they exhaust their attempts
+}
+
+//[Story 5, Tasks: 1, 2, 3, 4; Story 6, Tasks: 1, 2] adds question 7 that uses a gussing game with a series of user inputs and feedback; game terminates in the middle if user answers correctly - user gets 6 attempts
+
+alert('Let\'s play a new game I like to call "Holloween Delights". Can you guess some of my favorite Holloween treats? You\'ll have exactly 6 guesses to see if you can guess one from my list. Click "OK" when you\'re ready to begin.');
+
+// Declare variables and array
+var halloweenFavs = ['snickers', 'twizzlers', 'nerds', 'starburst', 'skittles'];
+var allowedAttemptsForQuestion7 = 6;
+var answeredQ7Correctly = false;
+
+for(var j=0; j <= allowedAttemptsForQuestion7; j++){
+  if(j === allowedAttemptsForQuestion7){    //checks to see if the user has exhausted all attempts and provides appropriate message if true
+    alert('Those were some great guesses but you have exhausted your guesses. My favorite treats are Snickers, Twizzlers, Nerds, Starburst, and Skittles. Thanks for playing!');
+    break;
+  }
+  
+  var seventhQuestion = (prompt('Guess one of my favorite Holloween treats?'));
+  for(var k=0; k<halloweenFavs.length; k++){      //this for loop checks the user input against the array values to determine if a correct answer was submitted
+    if(halloweenFavs[k] === seventhQuestion.toLowerCase()){    
+      alert('Correct, ' + halloweenFavs[k] + ' is one of my favorites. Happy Halloween!');
+      answeredQ7Correctly = true;
+      break;
+    }
+  }
+  if(answeredQ7Correctly){    // could have simplified by incorporating this in the above for loop but decided to create a separate alert message to close out if the user submitted a correct answer
+    alert('Looks like you knew me better than you think. My favorite treats are: Snickers, Twizzlers, Nerds, Starburst, and Skittles. Thanks for playing!');
+    correctAnswerCounter++;
+    break;
+  } else {    // if the user did not submit a correct guess then this alert is provided before we iterate the parent for loop for their next attempt
+    alert('Sorry, that\'s a great treat, but not one of my favorites. Let\'s try again.');
+  }
+} 
+  
+// [Story 6, Tasks: 1, 2] Add correct answer counter for the entire thing and provide an alert to tell user how many they got right
+alert('Congrats on getting through all the questions. Out of 7 questions, you got ' + correctAnswerCounter + ' correct. Cheers!');
+
+//[Story 2, Task 3] display user's name in the final message to the user
+alert('Thank you for stopping by ' + userName + '! I hope you enjoyed learning a little about me. Have a great day!');
+
